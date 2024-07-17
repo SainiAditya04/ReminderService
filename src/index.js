c0819e2fc421c420203862754d137c5db236b274
Aditya Saini <aditya.saini.ug20@nsut.ac.in>
2024-07-17 22:30:13 +0530

    add notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const app = express();
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require("./services/email-service");
 
 const startServer = () => {
     app.use(express.json());
 
     app.listen(PORT, (req, res) => {
         console.log(`server is started at PORT: ${PORT}`);
-
-        sendBasicEmail(
-            'support@admin.com',
-            'qriocity01@gmail.com',
-            'This is a test email',
-            'Hey there, hope you are doing fine ?'
-        );
     });
 }
 
